679a9e50200311ff5ff5ce58a34a3b8695a621e3
Dusan Klinec <dusan.klinec@gmail.com>
2021-03-14 18:05:18 +0100

    refactor analysis

--- ph4_walkingpad/analysis.py
+++ ph4_walkingpad/analysis.py
@@ -24,20 +24,14 @@ class StatsAnalysis:
                 dt = json.load(fh)
                 self.profile = Profile.from_data(dt)
 
-    def load_stats(self, limit=None):
-        for margins in self.parse_stats(limit):
+    def load_stats(self, limit=None, collect_details=False):
+        for margins in self.parse_stats(limit, collect_details=collect_details):
             self.loaded_margins.append(margins)
 
-    def parse_stats(self, limit=None):
+    def feed_records(self):
+        """Feed records from stats file in reversed order, one record per entry"""
         if not self.stats_file:
             return
-
-        # Load margins - boundary speed changes. In order to determine segments of the same speed.
-        last_rec = None
-        last_rec_diff = None
-        in_record = False
-        num_done = 0
-        margins = []
 
         with open(self.stats_file) as fh:
             reader = reverse_file(fh)
@@ -52,69 +46,91 @@ class StatsAnalysis:
                 except Exception as e:
                     continue
 
-                if not self.last_record:
-                    self.last_record = js
+                yield js
 
-                if js['speed'] != 0:
-                    in_record = True
+    def analyze_records_margins(self, records, limit=None, collect_details=False):
+        # Load margins - boundary speed changes. In order to determine segments of the same speed.
+        last_rec = None
+        last_rec_diff = None
+        in_record = False
+        num_done = 0
+        margins = []
+        sub_records = []
 
-                if not last_rec_diff or not in_record:
-                    last_rec_diff = js
-                    last_rec = js
-                    if not in_record:
-                        margins = [js]
-                    else:
-                        margins.append(js)
-                    continue
+        for js in records:
+            if not self.last_record:
+                self.last_record = js
 
-                time_diff = last_rec['time'] - js['time']
-                steps_diff = last_rec['steps'] - js['steps']
-                dist_diff = last_rec['dist'] - js['dist']
-                rtime_diff = last_rec['rec_time'] - js['rec_time']
-                time_to_rtime = abs(time_diff - rtime_diff)
+            if js['speed'] != 0:
+                in_record = True
 
-                breaking = time_diff < 0 or steps_diff < 0 or dist_diff < 0 or rtime_diff < 0 or time_to_rtime > 5*60
-                stats_changed = False
-
-                if breaking:
-                    if margins:
-                        mm = margins[-1]
-                        mm['_breaking'] = breaking
-
-                if not breaking \
-                        and (last_rec_diff['speed'] != js['speed']
-                        or (breaking and last_rec_diff['speed'] != 0)
-                        or (js['speed'] == 0 and js['time'] == 0)):
-
-                    js['_breaking'] = breaking
-                    js['_ldiff'] = [time_diff, steps_diff, dist_diff, rtime_diff]
-                    if margins:
-                        mm = margins[-1]
-                        mm['_segment_time'] = last_rec_diff['time'] - js['time']
-                        mm['_segment_rtime'] = last_rec_diff['rec_time'] - js['rec_time']
-                        mm['_segment_dist'] = last_rec_diff['dist'] - js['dist']
-                        mm['_segment_steps'] = last_rec_diff['steps'] - js['steps']
-
-                    margins.append(js)
-                    stats_changed = True
-                    last_rec_diff = js
-
-                if (stats_changed and js['speed'] == 0 and js['time'] == 0) or breaking:
-                    # print("done", breaking, time_to_rtime, time_diff, steps_diff, dist_diff, rtime_diff, js)
-                    #logger.info(json.dumps(margins, indent=2))
-                    if margins:
-                        yield margins
-                        num_done += 1
-                        if limit and num_done >= limit:
-                            return
-
-                    margins = [js]
-                    in_record = False
-                    last_rec_diff = js
-                    #last_rec = None
-
-                # last inst.
+            if not last_rec_diff or not in_record:
+                last_rec_diff = js
                 last_rec = js
+                sub_records = []
+                if not in_record:
+                    margins = [js]
+                else:
+                    margins.append(js)
+                continue
+
+            time_diff = last_rec['time'] - js['time']
+            steps_diff = last_rec['steps'] - js['steps']
+            dist_diff = last_rec['dist'] - js['dist']
+            rtime_diff = last_rec['rec_time'] - js['rec_time']
+            time_to_rtime = abs(time_diff - rtime_diff)
+
+            breaking = time_diff < 0 or steps_diff < 0 or dist_diff < 0 or rtime_diff < 0 or time_to_rtime > 5*60
+            stats_changed = False
+
+            if in_record and collect_details:
+                sub_records.append(dict(js))
+
+            if breaking:
+                if margins:
+                    mm = margins[-1]
+                    mm['_breaking'] = breaking
+
+            if not breaking \
+                    and (last_rec_diff['speed'] != js['speed']
+                    or (breaking and last_rec_diff['speed'] != 0)
+                    or (js['speed'] == 0 and js['time'] == 0)):
+
+                js['_breaking'] = breaking
+                js['_ldiff'] = [time_diff, steps_diff, dist_diff, rtime_diff]
+                if margins:
+                    mm = margins[-1]
+                    mm['_segment_time'] = last_rec_diff['time'] - js['time']
+                    mm['_segment_rtime'] = last_rec_diff['rec_time'] - js['rec_time']
+                    mm['_segment_dist'] = last_rec_diff['dist'] - js['dist']
+                    mm['_segment_steps'] = last_rec_diff['steps'] - js['steps']
+                    if collect_details:
+                        mm['_records'] = sub_records[:-1]
+                        sub_records = [dict(js)]
+
+                margins.append(js)
+                stats_changed = True
+                last_rec_diff = js
+
+            if (stats_changed and js['speed'] == 0 and js['time'] == 0) or breaking:
+                # print("done", breaking, time_to_rtime, time_diff, steps_diff, dist_diff, rtime_diff, js)
+                # logger.info(json.dumps(margins, indent=2))
+                if margins:
+                    yield margins
+                    num_done += 1
+                    if limit and num_done >= limit:
+                        return
+
+                margins = [js]
+                in_record = False
+                last_rec_diff = js
+
+            # last inst.
+            last_rec = js
+
+    def parse_stats(self, limit=None, collect_details=False):
+        gen = self.feed_records()
+        return self.analyze_records_margins(gen, limit, collect_details=collect_details)
 
     def comp_calories(self, margins):
         # logger.debug(json.dumps(margins, indent=2))
